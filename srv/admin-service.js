5f3e70dd4e4da7192e6badcc1dce5f82aade6a41
Elisa Mamolo <elisa.mamolo@gmail.com>
2022-02-17 12:41:41 +0000

    empty custom handlers for admin and user services

---
+++ srv/admin-service.js
@@ -0,0 +1,16 @@
+//const cds = require('@sap/cds')
+module.exports = async function (){
+
+  const db = await cds.connect.to('db') // connect to database service
+  const { Albums } = db.entities         // get reflected definitions
+
+  // Reduce stock of ordered albums if available stock suffices
+  this.on ('submitOrder', async req => {
+    const {album,quantity} = req.data
+    const n = await UPDATE (Books, book)
+      .with ({ stock: {'-=': quantity }})
+      .where ({ stock: {'>=': quantity }})
+    n > 0 || req.error (409,`${quantity} exceeds stock for album #${album}`)
+  })
+
+}
