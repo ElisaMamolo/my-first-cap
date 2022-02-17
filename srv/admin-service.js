aa77725db415705726a7c214613d4f81f2418019
Elisa Mamolo <elisa.mamolo@gmail.com>
2022-02-17 14:17:37 +0000

    reafctor admin service for handling entity Albums

--- srv/admin-service.js
+++ srv/admin-service.js
@@ -1,4 +1,4 @@
-//const cds = require('@sap/cds')
+const cds = require('@sap/cds')
 module.exports = async function (){
 
   const db = await cds.connect.to('db') // connect to database service
@@ -7,7 +7,7 @@ module.exports = async function (){
   // Reduce stock of ordered albums if available stock suffices
   this.on ('submitOrder', async req => {
     const {album,quantity} = req.data
-    const n = await UPDATE (Books, book)
+    const n = await UPDATE (Album, book)
       .with ({ stock: {'-=': quantity }})
       .where ({ stock: {'>=': quantity }})
     n > 0 || req.error (409,`${quantity} exceeds stock for album #${album}`)
